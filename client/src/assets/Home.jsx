de7820ee395bf0b6e70a6c52b1c900c5d900195f
unknown <saifkhanali101@gmail.com>
2024-06-09 19:52:19 +0530

    temp commit

--- client/src/assets/Home.jsx
+++ client/src/assets/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import styles from "../styles/HomeStyles.module.css";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
@@ -40,113 +40,126 @@ export default function Home() {
   const [exceedTDEE, setExceedTDEE] = useState(false);
   const [exceedFiber, setExceedFiber] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [recipeItems, setRecipeItems] = useState([]);
   // Fruits list
-  const fruitItems = [
-    { id: 1, name: "Apple" },
-    { id: 2, name: "Banana" },
-    { id: 27, name: "Banana Flower" },
-    { id: 26, name: "Cranberry" },
-    { id: 17, name: "Custard Apple" },
-    { id: 23, name: "Dates" },
-    { id: 18, name: "Dragon Fruit" },
-    { id: 14, name: "Jackfruit" },
-    { id: 20, name: "Jamun" },
-    { id: 21, name: "Fig (Anjeer)" },
-    { id: 11, name: "Guava" },
-    { id: 24, name: "Guavas" },
-    { id: 9, name: "Kiwi" },
-    { id: 15, name: "Chikoo (Sapota)" },
-    { id: 16, name: "Lychee" },
-    { id: 8, name: "Mango" },
-    { id: 19, name: "Mulberry" },
-    { id: 3, name: "Orange" },
-    { id: 13, name: "Pomegranate" },
-    { id: 12, name: "Papaya" },
-    { id: 10, name: "Peach" },
-    { id: 7, name: "Pineapple" },
-    { id: 4, name: "Grapes" },
-    { id: 25, name: "Tamarind" },
-    { id: 6, name: "Watermelon" },
-    { id: 5, name: "Strawberry" },
-  ];
+  const fruitItems = useMemo(
+    () => [
+      { id: 1, name: "Apple" },
+      { id: 2, name: "Banana" },
+      { id: 27, name: "Banana Flower" },
+      { id: 26, name: "Cranberry" },
+      { id: 17, name: "Custard Apple" },
+      { id: 23, name: "Dates" },
+      { id: 18, name: "Dragon Fruit" },
+      { id: 14, name: "Jackfruit" },
+      { id: 20, name: "Jamun" },
+      { id: 21, name: "Fig (Anjeer)" },
+      { id: 11, name: "Guava" },
+      { id: 24, name: "Guavas" },
+      { id: 9, name: "Kiwi" },
+      { id: 15, name: "Chikoo (Sapota)" },
+      { id: 16, name: "Lychee" },
+      { id: 8, name: "Mango" },
+      { id: 19, name: "Mulberry" },
+      { id: 3, name: "Orange" },
+      { id: 13, name: "Pomegranate" },
+      { id: 12, name: "Papaya" },
+      { id: 10, name: "Peach" },
+      { id: 7, name: "Pineapple" },
+      { id: 4, name: "Grapes" },
+      { id: 25, name: "Tamarind" },
+      { id: 6, name: "Watermelon" },
+      { id: 5, name: "Strawberry" },
+    ],
+    []
+  );
 
   // Vegetables list
-  const vegetableItems = [
-    { id: 50, name: "Artichoke" },
-    { id: 49, name: "Asparagus" },
-    { id: 35, name: "Bell Pepper" },
-    { id: 42, name: "Beetroot" },
-    { id: 53, name: "Brussels Sprouts" },
-    { id: 34, name: "Broccoli" },
-    { id: 43, name: "Capsicum" },
-    { id: 366, name: "Cabbage" },
-    { id: 31, name: "Carrot" },
-    { id: 32, name: "Cucumber" },
-    { id: 57, name: "Coriander" },
-    { id: 38, name: "Eggplant (Brinjal)" },
-    { id: 55, name: "Garlic" },
-    { id: 56, name: "Ginger" },
-    { id: 47, name: "Green Beans" },
-    { id: 52, name: "Kale" },
-    { id: 40, name: "Lettuce" },
-    { id: 45, name: "Okra (Ladyfinger)" },
-    { id: 30, name: "Onion" },
-    { id: 28, name: "Potato" },
-    { id: 48, name: "Peas" },
-    { id: 54, name: "Pumpkin" },
-    { id: 41, name: "Radish" },
-    { id: 36, name: "Spinach" },
-    { id: 44, name: "Sweet Potato" },
-    { id: 511, name: "Turnip" },
-    { id: 39, name: "Zucchini" },
-  ];
+  const vegetableItems = useMemo(
+    () => [
+      { id: 50, name: "Artichoke" },
+      { id: 49, name: "Asparagus" },
+      { id: 35, name: "Bell Pepper" },
+      { id: 42, name: "Beetroot" },
+      { id: 53, name: "Brussels Sprouts" },
+      { id: 34, name: "Broccoli" },
+      { id: 43, name: "Capsicum" },
+      { id: 366, name: "Cabbage" },
+      { id: 31, name: "Carrot" },
+      { id: 32, name: "Cucumber" },
+      { id: 57, name: "Coriander" },
+      { id: 38, name: "Eggplant (Brinjal)" },
+      { id: 55, name: "Garlic" },
+      { id: 56, name: "Ginger" },
+      { id: 47, name: "Green Beans" },
+      { id: 52, name: "Kale" },
+      { id: 40, name: "Lettuce" },
+      { id: 45, name: "Okra (Ladyfinger)" },
+      { id: 30, name: "Onion" },
+      { id: 28, name: "Potato" },
+      { id: 48, name: "Peas" },
+      { id: 54, name: "Pumpkin" },
+      { id: 41, name: "Radish" },
+      { id: 36, name: "Spinach" },
+      { id: 44, name: "Sweet Potato" },
+      { id: 511, name: "Turnip" },
+      { id: 39, name: "Zucchini" },
+    ],
+    []
+  );
 
   // Cereals list
-  const cerealItems = [
-    { id: 68, name: "Amaranth" },
-    { id: 1002, name: "Barley" },
-    { id: 60, name: "Barley" },
-    { id: 51, name: "Bajra" },
-    { id: 67, name: "Buckwheat" },
-    { id: 711, name: "Couscous" },
-    { id: 79, name: "Cornmeal" },
-    { id: 626, name: "Maize (Corn)" },
-    { id: 82, name: "Flaxseeds" },
-    { id: 76, name: "Jowar" },
-    { id: 77, name: "Jowar" },
-    { id: 63, name: "Rye" },
-    { id: 58, name: "Rice" },
-    { id: 71, name: "Semolina" },
-    { id: 75, name: "Soybeans" },
-    { id: 70, name: "Spelt" },
-    { id: 83, name: "Chia Seeds" },
-    { id: 62, name: "Sunflower Seeds" },
-    { id: 84, name: "Sunflower Seeds" },
-    { id: 69, name: "Farro" },
-    { id: 65, name: "Millet" },
-    { id: 64, name: "Quinoa" },
-    { id: 73, name: "Wild Rice" },
-    { id: 80, name: "Brown Rice" },
-    { id: 81, name: "Broken Wheat Dalia" },
-    { id: 66, name: "Sorghum (Jowar)" },
-    { id: 59, name: "Wheat" },
-  ];
+  const cerealItems = useMemo(
+    () => [
+      { id: 68, name: "Amaranth" },
+      { id: 1002, name: "Barley" },
+      { id: 60, name: "Barley" },
+      { id: 51, name: "Bajra" },
+      { id: 67, name: "Buckwheat" },
+      { id: 711, name: "Couscous" },
+      { id: 79, name: "Cornmeal" },
+      { id: 626, name: "Maize (Corn)" },
+      { id: 82, name: "Flaxseeds" },
+      { id: 76, name: "Jowar" },
+      { id: 77, name: "Jowar" },
+      { id: 63, name: "Rye" },
+      { id: 58, name: "Rice" },
+      { id: 71, name: "Semolina" },
+      { id: 75, name: "Soybeans" },
+      { id: 70, name: "Spelt" },
+      { id: 83, name: "Chia Seeds" },
+      { id: 62, name: "Sunflower Seeds" },
+      { id: 84, name: "Sunflower Seeds" },
+      { id: 69, name: "Farro" },
+      { id: 65, name: "Millet" },
+      { id: 64, name: "Quinoa" },
+      { id: 73, name: "Wild Rice" },
+      { id: 80, name: "Brown Rice" },
+      { id: 81, name: "Broken Wheat Dalia" },
+      { id: 66, name: "Sorghum (Jowar)" },
+      { id: 59, name: "Wheat" },
+    ],
+    []
+  );
   // Pulse items
-  const pulseItems = [
-    { id: 92, name: "Arhar Dal" },
-    { id: 87, name: "Black Beans" },
-    { id: 86, name: "Chickpeas" },
-    { id: 94, name: "Chana Dal" },
-    { id: 90, name: "Green Peas" },
-    { id: 97, name: "Horse Gram" },
-    { id: 88, name: "Kidney Beans" },
-    { id: 85, name: "Lentils" },
-    { id: 93, name: "Masoor Dal" },
-    { id: 96, name: "Matki" },
-    { id: 95, name: "Pigeon Peas" },
-    { id: 91, name: "Urad Dal" },
-  ];
+  const pulseItems = useMemo(
+    () => [
+      { id: 92, name: "Arhar Dal" },
+      { id: 87, name: "Black Beans" },
+      { id: 86, name: "Chickpeas" },
+      { id: 94, name: "Chana Dal" },
+      { id: 90, name: "Green Peas" },
+      { id: 97, name: "Horse Gram" },
+      { id: 88, name: "Kidney Beans" },
+      { id: 85, name: "Lentils" },
+      { id: 93, name: "Masoor Dal" },
+      { id: 96, name: "Matki" },
+      { id: 95, name: "Pigeon Peas" },
+      { id: 91, name: "Urad Dal" },
+    ],
+    []
+  );
   // Inside your handleLogButtonClick function in the React component
   const handleLogout = () => {
     logout();
@@ -307,7 +320,7 @@ export default function Home() {
     console.log("foodGrams before calculation:", foodGrams);
     console.log("selectedFoods before calculation:", selectedFoods);
     const apiUrl = "https://trackapi.nutritionix.com/v2/natural/nutrients";
-    const apiKey = "5b85e7232c4489df93c3b09be08a0af8";
+    const apiKey = "1ee1a126dec6d98fac2c381760dffd47";
 
     try {
       const promises = selectedFoods.map(async (food) => {
@@ -369,6 +382,34 @@ export default function Home() {
       foodItems.reduce((total, food) => total + food.nf_dietary_fiber, 0)
     );
   }, [foodItems]);
+
+  useEffect(() => {
+    const selected = [
+      ...fruitItems,
+      ...vegetableItems,
+      ...cerealItems,
+      ...pulseItems,
+    ].filter((item) =>
+      selectedFoods.some((selected) => selected.id === item.id)
+    );
+    setSelectedItems(selected);
+  }, [selectedFoods, fruitItems, vegetableItems, cerealItems, pulseItems]);
+
+  const handleCheckboxChangeForRecipe = (item) => {
+    setRecipeItems((prevRecipeItems) => {
+      if (prevRecipeItems.some((recipeItem) => recipeItem.id === item.id)) {
+        return prevRecipeItems.filter(
+          (recipeItem) => recipeItem.id !== item.id
+        );
+      } else {
+        console.log("Recipe items:", recipeItems); // Log the recipeItems array
+        return [...prevRecipeItems, item];
+      }
+    });
+  };
+  useEffect(() => {
+    console.log("Recipe items:", recipeItems); // Log the updated recipeItems array
+  }, [recipeItems]);
 
   return (
     <>
@@ -735,49 +776,26 @@ export default function Home() {
         </div>
       </div>
 
-      {/* <div>
-        <Link
-          to={{
-            pathname: "/nutrition",
-            state: {
-              totalCarbs: totalCarbsArray,
-              totalProtein: totalProteinArray,
-            },
-          }}
-        >
-          <button>Performance</button>
-        </Link>{" "}
-      </div> */}
-      <div>
-        {/* <form className={styles.recipeForm} onSubmit={handleSubmitGpt}>
-          <div>
-            <div>
-              <label>Recipe for the items</label>
-            </div>
-            
-            <div>
-              <button type="submit">Recipe</button>
-            </div>
-          </div>
-        </form> */}
-        {/* <div className={styles.foodSelectedParent}>
-          <div className={styles.foodSelectedChild}>
-            {foodSelected.map((item) => (
-              <div key={item.id}>
+      <div className={styles.selectedItems}>
+        <h3>Selected Food Items:</h3>
+        {selectedItems.length > 0 ? (
+          <ul>
+            {selectedItems.map((item) => (
+              <li key={item.id}>
                 <input
                   type="checkbox"
-                  name={item.name}
-                  id={item.id}
-                  onChange={() => recipeAdd(item.name)}
+                  checked={recipeItems.some(
+                    (recipeItem) => recipeItem.id === item.id
+                  )}
+                  onChange={() => handleCheckboxChangeForRecipe(item)}
                 />
-                <label htmlFor={item.id}>{item.name}</label>
-              </div>
+                <label htmlFor={`recipe-${item.id}`}>{item.name}</label>
+              </li>
             ))}
-          </div>
-        </div> */}
-        {/* <div>
-          <p>{response}</p>
-        </div> */}
+          </ul>
+        ) : (
+          <h3>Please select the food items</h3>
+        )}
       </div>
     </>
   );
